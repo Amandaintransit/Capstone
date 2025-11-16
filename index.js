0603d67df41adf5f9197068e3a0c426aca879fe9
amanda-spears <amanda_spears@hotmail.com>
2025-11-15 19:57:10 -0500

    figuring out the template literals for page 2

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 
-const custodianA = document.getElementById("custodianA");
-const custodianB = document.getElementById("custodianB")
 
+const params = new URLSearchParams(window.location.search);
+
+document.getElementById("custodianA").textContent = params.get("custodianA");
+
+document.getElementById("custodianB").textContent = params.get("custodianB");
+
+/*const custodianA = params.get("CustodianA");
+const custodianB = params.get("CustodianB");
+const numberOfChildren = params.get("NumberOfChildren");
+
+document.getElementById("custodianAinfo").value = custodianA;
+
+
+
+
+    const resultsList = document.getElementById('results')
+    new URLSearchParams(window.location.search).forEach((value, name)=> {
+        resultsList.append(`${name}: ${value}`)
+        resultsList.append(document.createElement('br'))
+    })
+*/
